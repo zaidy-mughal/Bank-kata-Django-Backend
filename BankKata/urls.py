95c71a664fa0a757c972a3fbf735f5eb9447e4fd
Muhammad Zaid Amjad <zaidamjadse@gmail.com>
2024-07-29 01:42:04 +0500

    Updated

--- BankKata/urls.py
+++ BankKata/urls.py
@@ -4,8 +4,10 @@ from bank_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accountstatement/',views.statement),
-    path('accountstatement/<int:pk>',views.statement),
-    path('deposit/',views.depositView),
-    path('deposit/<int:pk>',views.depositView),
+    path('accountstatement/',views.StatementView.as_view(),name="Account Statement"),
+    path('accountstatement/<int:pk>',views.StatementView.as_view(),name="Account Statement"),
+    path('deposit/',views.DepositView.as_view(),name="Deposit Amount"),
+    path('deposit/<int:pk>',views.DepositView.as_view(),name="Deposit Amount"),
+    path('withdraw/',views.WithdrawView.as_view(),name="Withdraw Amount"),
+    path('withdraw/<int:pk>',views.WithdrawView.as_view(),name="Withdraw Amount"),
 ]
--- bank_app/views.py
+++ bank_app/views.py
@@ -1,21 +1,23 @@
-
 from django.shortcuts import render
 from .serializers import AccountSerializer,MovementSerializer
 from .models import Account, Movement
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
-@api_view(["GET","PATCH"])
-def depositView(request,pk=None):
-    if request.method == "GET":
-        if pk is not None:
-            account = Account.objects.get(pk=pk)
-        account = Account.objects.all()
-        serializer = AccountSerializer(account,many=True)
+
+class DepositView(APIView):
+    def get(self,request,pk=None,format=None):
+        if pk is None:
+            account = Account.objects.all()
+            serializer = AccountSerializer(account,many=True)
+        account = Account.objects.get(pk=pk)
+        serializer = AccountSerializer(account)
         return JsonResponse(serializer.data,safe=False)
 
-    elif request.method == "PATCH":
+
+    def post(self,request,pk=None,format=None):
         try:
             account = Account.objects.get(pk=pk)
             serializer = AccountSerializer(account,data=request.data,partial=True)
@@ -41,10 +43,55 @@ def depositView(request,pk=None):
             return JsonResponse({'msg':'Invalid Account ID'})
 
 
-@api_view(["GET"])
-def statement(request,pk=None):
-    if pk is not None:
-        statement = Movement.objects.filter(account_id=pk)
-        serializer = MovementSerializer(statement,many=True)
+
+class WithdrawView(APIView):
+    def get(self,request,pk=None,format=None):
+        if pk is None:
+            account = Account.objects.all()
+            serializer = AccountSerializer(account,many=True)
+        account = Account.objects.get(pk=pk)
+        serializer = AccountSerializer(account)
         return JsonResponse(serializer.data,safe=False)
-    return JsonResponse({'msg':'Provide Account ID in the URL'})
+
+
+    def post(self,request,pk=None,format=None):
+        try:
+            account = Account.objects.get(pk=pk)
+            serializer = AccountSerializer(account,data=request.data,partial=True)
+            requestamount = int(request.data.get('amount',0))
+
+            if requestamount <= account.balance:
+                totalBalance = account.balance - requestamount
+            else:
+                return JsonResponse({'msg':'Low Balance!'})
+            
+            account.balance = totalBalance
+            movement = {
+                "movement_type": "Withdraw",
+                "account": account.id,
+                "amount": -requestamount,
+                "balance": totalBalance,
+            }
+            mov_serializer = MovementSerializer(data=movement)
+
+            if serializer.is_valid() and mov_serializer.is_valid():
+                serializer.save()
+                mov_serializer.save()
+                return JsonResponse({'msg':'Amount Withdrawed'})
+            else:
+                return JsonResponse([serializer.errors,mov_serializer.errors],safe=False)
+            
+        except Account.DoesNotExist:
+            return JsonResponse({'msg':'Invalid Account ID'})
+
+
+
+
+
+class StatementView(APIView):
+    def get(self,request,pk=None,format=None):
+        if pk is not None:
+            statement = Movement.objects.filter(account_id=pk)
+            serializer = MovementSerializer(statement,many=True)
+            return JsonResponse(serializer.data,safe=False)
+        return JsonResponse({'msg':'Provide Account ID in the URL'})
